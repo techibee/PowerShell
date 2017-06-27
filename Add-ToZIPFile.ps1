13a48191afe3a45f6ac09e23af081e53d39d9529
techibee <techibee.author@gmail.com>
2017-06-27 20:11:54 +0800

    Add files via upload

---
+++ Add-ToZIPFile.ps1
@@ -0,0 +1,17 @@
+﻿[CmdletBinding()]
+Param(
+    [string]$ZIPFileName,
+    [string]$NewFileToAdd
+)
+
+try {
+    [Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem') | Out-Null
+    $zip = [System.IO.Compression.ZipFile]::Open($ZIPFileName,"Update")
+    $FileName = [System.IO.Path]::GetFileName($NewFileToAdd)
+    [System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip,$NewFileToAdd,$FileName,"Optimal") | Out-Null
+    $Zip.Dispose()
+    Write-Host "Successfully added $NewFileToAdd to $ZIPFileName "
+} catch {
+    Write-Warning "Failed to add $NewFileToAdd to $ZIPFileName . Details : $_"
+
+}
